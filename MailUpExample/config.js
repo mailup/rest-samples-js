a839de9d4065823ebca97ed487d1b185b4a649ac
viktoryia.ryzhkovich <viktoryia.ryzhkovich@itibo.com>
2019-01-25 12:08:42 +0300

    updated files

---
+++ MailUpExample/config.js
@@ -0,0 +1,8 @@
+var ConfigStrings = {
+
+    CLIENT_ID: "6a324fe1-5bfa-4b5f-815b-f05d2d7efcd8",
+    CLIENT_SECRET: "f05b406e-deb8-408d-8cfd-8f2a92111dde",
+    CALLBACK_URI: "http://127.0.0.1/index.html",
+
+    LIST_ID: 1
+}
