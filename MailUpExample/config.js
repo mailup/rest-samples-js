916d30b1da64646a8a85f5394627145dc4d63452
NWeb <acquisti@mailup.com>
2019-01-25 16:22:20 +0100

    Removed invalid credentials

--- MailUpExample/config.js
+++ MailUpExample/config.js
@@ -1,7 +1,7 @@
 var ConfigStrings = {
 
-    CLIENT_ID: "6a324fe1-5bfa-4b5f-815b-f05d2d7efcd8",
-    CLIENT_SECRET: "f05b406e-deb8-408d-8cfd-8f2a92111dde",
+    CLIENT_ID: "<YOUR_CLIENT_ID>",
+    CLIENT_SECRET: "<YOUR_CLIENT_SECRET>",
     CALLBACK_URI: "http://127.0.0.1/index.html",
 
     LIST_ID: 1
